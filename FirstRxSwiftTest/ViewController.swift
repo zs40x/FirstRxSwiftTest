004fe2e66eb05539d3a4c73eec6fa1cc1c73796a
Stefan Mehnert <StefanMehnert@gmail.com>
2017-04-08 22:39:23 +0200

    Initial Commit

---
+++ FirstRxSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstRxSwiftTest
+//
+//  Created by Stefan Mehnert on 08.04.17.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
