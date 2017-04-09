f808ab8d10823e560fecee5dea45e12809296043
Stefan Mehnert <StefanMehnert@gmail.com>
2017-04-09 10:13:10 +0200

    Added a simple publishSubject example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -3,7 +3,13 @@
 import UIKit
 import RxSwift
 
+enum Errors: Error {
+    case anError
+}
+
 let disposeBag = DisposeBag()
+
+//# Simple observable example
 let observeable = Observable<Int>.range(start: 1, count: 10)
 
 observeable.subscribe(
@@ -21,3 +27,23 @@ observeable.subscribe(
         print("Disposed")
     }
 ).addDisposableTo(disposeBag)
+
+
+//# Simple subject example
+let subject = PublishSubject<String>()
+
+subject.subscribe(
+    onNext: { string in
+        print(string)
+    },
+    onError: { error in
+        print("Error: \(error)")
+    },
+    onDisposed: {
+        print("String subject disposed")
+    }
+).addDisposableTo(disposeBag)
+
+subject.on(.next("Hello world from a string subject"))
+subject.on(.next("sdf"))
+subject.on(.error(Errors.anError))
